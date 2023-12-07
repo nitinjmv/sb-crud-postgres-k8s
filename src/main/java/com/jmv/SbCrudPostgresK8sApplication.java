3af3468dc748f39aaa549037d5385e7df22d8170
nitinjmv <nitinjmv@gmail.com>
2023-12-07 11:03:01 +0000

    Initial commit

---
+++ src/main/java/com/jmv/SbCrudPostgresK8sApplication.java
@@ -0,0 +1,13 @@
+package com.jmv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbCrudPostgresK8sApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbCrudPostgresK8sApplication.class, args);
+	}
+
+}
